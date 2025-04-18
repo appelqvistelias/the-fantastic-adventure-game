c7e5aef4817300c03cad8971c9faedb3d13cfc7d
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-04-18 14:33:01 +0200

    added ASCII art

--- the-fantastic-adventure-game/SceneTextContent/StartText.cs
+++ the-fantastic-adventure-game/SceneTextContent/StartText.cs
@@ -5,11 +5,33 @@ namespace the_fantastic_adventure_game.Text
         public void ShowIntro()
         {
             Console.Clear();
-            Console.WriteLine("Welcome to The Fantastic Adventure Game!");
+
+            Console.WriteLine(@"
+ __          __  _                            _                                  
+ \ \        / / | |                          | |                                 
+  \ \  /\  / /__| | ___ ___  _ __ ___   ___  | |_ ___                            
+   \ \/  \/ / _ \ |/ __/ _ \| '_ ` _ \ / _ \ | __/ _ \                           
+    \  /\  /  __/ | (_| (_) | | | | | |  __/ | || (_) |                          
+  ___\/__\/ \___|_|\___\___/|_| |_| |_|\___|  \__\___/                           
+ |__   __| |              /\                       (_)                           
+    | |  | |__   ___     /  \   _ __ ___   __ _ _____ _ __   __ _                
+    | |  | '_ \ / _ \   / /\ \ | '_ ` _ \ / _` |_  / | '_ \ / _` |               
+    | |  | | | |  __/  / ____ \| | | | | | (_| |/ /| | | | | (_| |               
+    |_|  |_| |_|\___| /_/    \_\_| |_| |_|\__,_/___|_|_|_|_|\__, |               
+     /\      | |               | |                   / ____| __/ |               
+    /  \   __| |_   _____ _ __ | |_ _   _ _ __ ___  | |  __ |___/ _ __ ___   ___ 
+   / /\ \ / _` \ \ / / _ \ '_ \| __| | | | '__/ _ \ | | |_ |/ _` | '_ ` _ \ / _ \
+  / ____ \ (_| |\ V /  __/ | | | |_| |_| | | |  __/ | |__| | (_| | | | | | |  __/
+ /_/    \_\__,_| \_/ \___|_| |_|\__|\__,_|_|  \___|  \_____|\__,_|_| |_| |_|\___|
+                                                                                 
+");
+
             Console.WriteLine("You are about to embark on a fantastic adventure!");
+            Console.WriteLine();
             Console.WriteLine("1. Start the adventure");
             Console.WriteLine("2. Exit the game");
         }
+
 
         public void ShowBeginning()
         {
