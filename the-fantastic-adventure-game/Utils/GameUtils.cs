9d6f394b626181686cf9ad7205e436625f6adead
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-04-18 14:56:42 +0200

    added methods for inventory

--- the-fantastic-adventure-game/Utils/GameUtils.cs
+++ the-fantastic-adventure-game/Utils/GameUtils.cs
@@ -1,7 +1,33 @@
+using the_fantastic_adventure_game.Items;
+
 namespace the_fantastic_adventure_game.Utils
+
 {
     public static class GameUtils
     {
+        private static List<Item> Inventory = new();
+
+        public static void AddToInventory(Item item)
+        {
+            Inventory.Add(item);
+            Console.WriteLine($"\nYou received: {item.Name} - {item.Description}");
+        }
+
+        public static void ShowInventory()
+        {
+            Console.WriteLine("\n--- Your Inventory ---");
+            if (Inventory.Count == 0)
+            {
+                Console.WriteLine("It's empty.");
+            }
+            else
+            {
+                foreach (var item in Inventory)
+                {
+                    Console.WriteLine($"- {item.Name}: {item.Description}");
+                }
+            }
+        }
         public static void ShowInvalidChoice()
         {
             Console.WriteLine("Invalid choice, please try again.");
