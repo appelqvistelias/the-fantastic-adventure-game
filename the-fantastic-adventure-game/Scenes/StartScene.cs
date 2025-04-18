01a211fed1b30e223d3ba7c96f83d4af8ac87b45
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-04-18 19:48:07 +0200

    added condition to path choice based on items

--- the-fantastic-adventure-game/Scenes/StartScene.cs
+++ the-fantastic-adventure-game/Scenes/StartScene.cs
@@ -2,71 +2,128 @@ using the_fantastic_adventure_game.Text;
 using the_fantastic_adventure_game.Utils;
 using the_fantastic_adventure_game.Scenes;
 
-namespace the_fantastic_adventure_game.Scene
+namespace the_fantastic_adventure_game.Scene;
+
+public class StartScene
 {
-    public class StartScene
+    private readonly StartText startText = new StartText();
+
+    public void StartGame()
     {
-        private readonly StartText startText = new StartText();
+        bool gameRunning = true;
 
-        public void StartGame()
+        while (gameRunning)
         {
-            bool gameRunning = true;
+            startText.ShowIntro();
+            string? choice = Console.ReadLine()?.Trim();
 
-            while (gameRunning)
+            switch (choice)
             {
-                startText.ShowIntro();
-                string? choice = Console.ReadLine()?.Trim();
-
-                switch (choice)
-                {
-                    case "1":
-                        startText.ShowBeginning();
-                        SelectScene();
-                        break;
-                    case "2":
-                        GameUtils.ShowInventory();
-                        Console.WriteLine("Press any key to return to main menu");
-                        Console.ReadKey();
-                        break;
-                    case "3":
-                        GameUtils.ClearInventory();
-                        Console.WriteLine("Press any key to return to main menu");
-                        Console.ReadKey();
-                        break;
-                    case "4":
-                        GameUtils.ShowExitMessage();
-                        Environment.Exit(0);
-                        break;
-                    default:
-                        GameUtils.ShowInvalidChoice();
-                        break;
-                }
+                case "1":
+                    startText.ShowBeginning();
+                    SelectScene();
+                    break;
+                case "2":
+                    GameUtils.ShowInventory();
+                    Console.WriteLine("Press any key to return to main menu");
+                    Console.ReadKey();
+                    break;
+                case "3":
+                    GameUtils.ClearInventory();
+                    Console.WriteLine("Press any key to return to main menu");
+                    Console.ReadKey();
+                    break;
+                case "4":
+                    GameUtils.ShowExitMessage();
+                    Environment.Exit(0);
+                    break;
+                default:
+                    GameUtils.ShowInvalidChoice();
+                    break;
             }
         }
+    }
 
-        private void SelectScene()
+    private void SelectScene()
+    {
+        bool selecting = true;
+
+        while (selecting)
         {
+            Console.Clear();
             Console.WriteLine("\nChoose a location to explore:");
             Console.WriteLine("1. The Village of Eldermoor");
             Console.WriteLine("2. The Dark Forest of Shadow Wood");
             Console.WriteLine("3. Murmurdeep, the Mysterious Cave");
             Console.WriteLine("4. The Murky Lake of Somberveil");
+            Console.WriteLine("5. Return to Main Menu");
 
-            int selection = GameUtils.GetValidInput(1, 4);
+            int selection = GameUtils.GetValidInput(1, 5);
 
             switch (selection)
             {
                 case 1:
-                    VillageScene.Play();
+                    if (GameUtils.HasItem("Aegis of the Shattered Sun"))
+                    {
+                        Console.WriteLine("\nYou’ve already completed The Village of Eldermoor.");
+                        Console.WriteLine("Press any key to choose another location.");
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        VillageScene.Play();
+                        selecting = false;
+                    }
+
                     break;
+
                 case 2:
-                    ForestScene.Play();
+                    if (GameUtils.HasItem("Mystical Forest Crystal Crown"))
+                    {
+                        Console.WriteLine("\nYou’ve already completed The Dark Forest of Shadow Wood.");
+                        Console.WriteLine("Press any key to choose another location.");
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        ForestScene.Play();
+                        selecting = false;
+                    }
+
                     break;
+
                 case 3:
-                    CaveScene.Play();
+                    if (GameUtils.HasItem("Shadowfang Blade"))
+                    {
+                        Console.WriteLine("\nYou’ve already completed Murmurdeep Caverns.");
+                        Console.WriteLine("Press any key to choose another location.");
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        CaveScene.Play();
+                        selecting = false;
+                    }
+
                     break;
+
                 case 4:
-                    LakeScene.Play();
+                    if (GameUtils.HasItem("Panoply of the Drowned King"))
+                    {
+                        Console.WriteLine("\nYou’ve already completed The Murky Lake of Somberveil.");
+                        Console.WriteLine("Press any key to choose another location.");
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        LakeScene.Play();
+                        selecting = false;
+                    }
+
+                    break;
+
+                case 5:
+                    selecting = false;
                     break;
             }
         }
